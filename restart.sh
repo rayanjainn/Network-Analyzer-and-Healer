438d3cea3170f3abd418a902396c1ab1f8fccc9b
Th3C0d3Mast3r <acharyadevesh1105@gmail.com>
2025-11-13 23:29:55 +0530

    small docker download change with ANSI coloring

--- restart.sh
+++ restart.sh
@@ -2,14 +2,23 @@
 # CLEANUP SCRIPT for Network Analyzer Stack
 # Use when automateSetup.sh fails midway or you want a full reset
 
+# ====== COLORS ======
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+RED="\033[0;31m"
+BLUE="\033[0;34m"
+BOLD="\033[1m"
+r="\033[0m"
+
+
 echo "-----------------------------------------"
-echo "[RESTARTING THE SYSTEM] - NETWORK ANALYZER CLEANUP"
+echo -e "${BOLD}[RESTARTING THE SYSTEM] - NETWORK ANALYZER CLEANUP${r}"
 echo "-----------------------------------------"
 
 set -e
 
 # Stop and remove containers
-echo "[DOCKER] - Removing running containers..."
+echo -e "${RED}[DOCKER] - Removing running containers...${e}"
 sudo docker rm -f loki grafana promtail 2>/dev/null || true
 
 # Remove docker network
@@ -20,21 +29,21 @@ if sudo docker network ls --format '{{.Name}}' | grep -q "^${NET_NAME}$"; then
 fi
 
 # Remove configuration and log directories
-echo "[FILESYSTEM] - Removing log-stack and Promtail directories..."
+echo -e "${RED}[FILESYSTEM] - Removing log-stack and Promtail directories...${r}"
 sudo rm -rf ~/log-stack
 sudo rm -rf /etc/promtail
 sudo rm -rf /var/log/remote
 
 # Restore rsyslog backup if exists
 if [ -f /etc/rsyslog.conf.bak ]; then
-  echo "[RSYSLOG] - Restoring backup config..."
+  echo -e "${RED}[RSYSLOG] - Restoring backup config...${r}"
   sudo mv /etc/rsyslog.conf.bak /etc/rsyslog.conf
 fi
 
 # Restart rsyslog service
-echo "[RSYSLOG] - Restarting service..."
+echo -e "${YELLOW}${BOLD}[RSYSLOG] - Restarting service...${r}${r}"
 sudo systemctl restart rsyslog || true
 
 echo "-----------------------------------------"
-echo "[SUCCESS] - Cleanup completed! System reset to initial state."
+echo -e "${GREEN}[${BOLD}SUCCESS] - Cleanup completed! System reset to initial state.${r}"
 echo "-----------------------------------------"
